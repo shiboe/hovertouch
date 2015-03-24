8d2de1877b81cac45860525036724cc513bcec3c
Spectra - Stephen Cave <stephen@spectrahealth.us>
2015-03-24 14:53:29 -0700

    Flesh out Readme and add code

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # hovertouch
 Corrects hover effects for touch devices when paired with CSS replacements
+
+## Usage
+Include hovertouch.js on your page, and call `hovertouch.init()`. After that, any user touch will apply and remove a `.hovertouch` class to an associated anchor link, button, or input. Now you can duplicate your CSS hovers as .hovertouch to cover mobile! Some wonkiness can occur with leaving `:hover` on a touch device, so this utility will also add a `.hoverable` class to `document.body` for devices detected with a mouse. This allows you to safely prefix your CSS :hover to prevent it from firing on non-mouse devices.
+
+## Example
+After including hovertouch.js...
+
+### Javascript
+You will run the initializer...
+```
+<script>hovertouch.init();</script>
+```
+
+### CSS
+And have included some CSS to fulfill your destiny...
+```
+.hoverable a:hover,
+a.hovertouch { color:red; }
+```
+Additionally, the following [SCSS mixin](http://sass-lang.com/guide) mixin will cover your bases automatically.
+```
+@mixin hover($sel, $attr) {
+  .hoverable $sel:hover,
+  $sel.hovertouch { $attr }
+}
+```
+
+## Dependencies
+This utility requires classList support. To fulfill this dependency on legacy browsers, you should have a polyfill loaded before hovertouch runs.
+
---
+++ hovertouch.js
@@ -0,0 +1,57 @@
+var hovertouch = (function(){
+
+  // our default elements
+  var touchables = ['A', 'BUTTON', 'INPUT'];
+
+  // windows uses seperate events, so we need a tester
+  var isWindows = !! window.navigator.msPointerEnabled;
+
+
+
+  // for assuredness and simplicity, we kill all things, all time
+  function unTouchAll(){
+    var touched = document.getElementsByClassName('hovertouch');
+    while( touched.length ) unTouch( touched[0] );
+  };
+
+  // our touching functions--basic class add/remove
+  function unTouch( el ){ el.classList.remove('hovertouch'); };
+  function touch( el ){ el.classList.add('hovertouch'); };
+
+  // we only want to hovertouch certain elements for sanity
+  function touchable( el ){
+    return touchables.indexOf(el.tagName) !== -1;
+  };
+
+
+
+  return {
+    init: function(options){
+
+      // use custom elements to hovertouch
+      if( options.elements ) {
+        touchables = options.elements.map(function(el) {
+          return el.toUpperCase();
+        });
+      }
+
+      // when a touch happens, let's hovertouch it!
+      document.body.addEventListener( isWindows ? 'MSPointerDown' : 'touchstart', function(e){
+        var source = e.target || e.srcElement;
+        if( touchable( source ) ) touch( source );
+      });
+
+      // when the touch has ended, so must the hovertouch
+      document.body.addEventListener( isWindows ? 'MSPointerUp' : 'touchend', unTouchAll );
+      document.body.addEventListener( isWindows ? 'MSPointerCancel' : 'touchcancel', unTouchAll );
+
+      var mousing = function(e) {
+        document.body.classList.add('hoverable');
+        document.body.removeEventListener('mouseover', mousing);
+      }
+
+      // lets also check for mouse so we can only enable hover as needed
+      document.body.addEventListener('mouseover', mousing);
+    }
+  }
+})();
